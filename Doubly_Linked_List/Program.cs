ba4c466fd11bed9d8ed7fb05ab33ebd3efd8e471
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-16 17:39:45 +0700

    Add a method to delete a specified data in the list

--- Doubly_Linked_List/Program.cs
+++ Doubly_Linked_List/Program.cs
@@ -85,5 +85,30 @@ namespace DoublyLinkedList
              is found then the function return true, otherwise false.*/
             return (current != null);
         }
+
+        public bool delNode(int rollNo)/*Deletes the specified node*/
+        {
+            Node previous, current;
+            previous = current = null;
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+            if (current == START)/*If the first node is to be deleted*/
+            {
+                START = START.next;
+                if (START != null)
+                    START.prev = null;
+                return true;
+            }
+            if (current.next == null)/*if the last node is to be deleted*/
+            {
+                previous.next = null;
+                return true;
+            }
+            /*If the node to be deleted is in between the list then the
+             following lines of code is executed*/
+            previous.next = current.next;
+            current.next.prev = previous;
+            return true;
+        }
     }
 }
