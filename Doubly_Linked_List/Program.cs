4eaa778d165f1ffa77a73ba8777fe3d3a1c378b9
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-16 17:30:13 +0700

    Add a method to use double linked list

--- Doubly_Linked_List/Program.cs
+++ Doubly_Linked_List/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 
 namespace DoublyLinkedList
 {
@@ -9,8 +10,68 @@ namespace DoublyLinkedList
          * its succeeding and preceding nodes
          * in terms of next and previous nodes.*/
         public int rollNumber;
-        public string namme;
+        public string name;
         public Node next;/*points to the succeeding node*/
         public Node prev;/*points to the preceeding node*/
+    }
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()/*Adds a new node*/
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            /*Checks if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START; current != null &&
+                rollNo >= current.rollNumber; previous = current,
+                current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            /*On the execution of the above for loop, prev and current
+             will point to those nodes between which the new node is to be inserted.*/
+            newnode.next = current;
+            newnode.prev = previous;
+
+            /*If the node is to be inserted at the end of the list.*/
+            if (current == null)
+            {
+                newnode.next = null;
+                previous.next = newnode;
+                return;
+            }
+            current.prev = newnode;
+            previous.next = newnode;
+        }
     }
 }
