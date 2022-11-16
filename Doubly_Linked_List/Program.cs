c306a8ac3069bf294d0c4415f3ce3e667e652ff0
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-16 16:59:52 +0700

    Declaring variable

--- Doubly_Linked_List/Program.cs
+++ Doubly_Linked_List/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+         * It consists of the information part and links to
+         * its succeeding and preceding nodes
+         * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string namme;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
+    }
+}
